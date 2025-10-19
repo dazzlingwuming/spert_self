afc035d0cceb44c273a761e4a3f944cff9519596
lihaodong <2409261729@qq.com>
2025-10-20 00:35:18 +0800

    数据样本产生

--- spert/sampling.py
+++ spert/sampling.py
@@ -6,59 +6,59 @@ from spert import util
 
 
 def create_train_sample(doc, neg_entity_count: int, neg_rel_count: int, max_span_size: int, rel_type_count: int):
-    encodings = doc.encoding
-    token_count = len(doc.tokens)
-    context_size = len(encodings)
+    encodings = doc.encoding#获取文本编码
+    token_count = len(doc.tokens)#获取文本中token的数量
+    context_size = len(encodings)#获取编码的长度
 
-    # positive entities
+    # positive entities 正样本的构建
     pos_entity_spans, pos_entity_types, pos_entity_masks, pos_entity_sizes = [], [], [], []
     for e in doc.entities:
-        pos_entity_spans.append(e.span)
-        pos_entity_types.append(e.entity_type.index)
-        pos_entity_masks.append(create_entity_mask(*e.span, context_size))
-        pos_entity_sizes.append(len(e.tokens))
+        pos_entity_spans.append(e.span)#获取实体的跨度，也就是起始和结束位置
+        pos_entity_types.append(e.entity_type.index)#获取实体的类型索引
+        pos_entity_masks.append(create_entity_mask(*e.span, context_size))#创建实体掩码
+        pos_entity_sizes.append(len(e.tokens))#获取实体的大小
 
     # positive relations
 
-    # collect relations between entity pairs
+    # collect relations between entity pairs 创建实体对之间的关系正样本，但是这里的建立是单关系的实体对
     entity_pair_relations = dict()
     for rel in doc.relations:
-        pair = (rel.head_entity, rel.tail_entity)
+        pair = (rel.head_entity, rel.tail_entity)#获取关系的头实体和尾实体组成的实体对
         if pair not in entity_pair_relations:
-            entity_pair_relations[pair] = []
+             entity_pair_relations[pair] = []
         entity_pair_relations[pair].append(rel)
 
-    # build positive relation samples
+    # build positive relation samples 构建正样本的关系
     pos_rels, pos_rel_spans, pos_rel_types, pos_rel_masks = [], [], [], []
     for pair, rels in entity_pair_relations.items():
-        head_entity, tail_entity = pair
-        s1, s2 = head_entity.span, tail_entity.span
-        pos_rels.append((pos_entity_spans.index(s1), pos_entity_spans.index(s2)))
+        head_entity, tail_entity = pair #获取实体对的头实体和尾实体
+        s1, s2 = head_entity.span, tail_entity.span #获取头实体和尾实体的跨度
+        pos_rels.append((pos_entity_spans.index(s1), pos_entity_spans.index(s2))) #将实体跨度转换为索引并添加到正样本关系列表中
         pos_rel_spans.append((s1, s2))
 
-        pair_rel_types = [r.relation_type.index for r in rels]
-        pair_rel_types = [int(t in pair_rel_types) for t in range(1, rel_type_count)]
+        pair_rel_types = [r.relation_type.index for r in rels]#获取实体对之间的关系类型索引，这里可能有多个关系类型
+        pair_rel_types = [int(t in pair_rel_types) for t in range(1, rel_type_count)]#将关系类型转换为多标签二进制表示，忽略类型0
         pos_rel_types.append(pair_rel_types)
-        pos_rel_masks.append(create_rel_mask(s1, s2, context_size))
+        pos_rel_masks.append(create_rel_mask(s1, s2, context_size))#创建关系掩码，对关系对应的文本区域进行掩码（两个实体之间的文本区域之间的所以位置为1）
 
-    # negative entities
+    # negative entities 负样本实体的构建
     neg_entity_spans, neg_entity_sizes = [], []
     for size in range(1, max_span_size + 1):
         for i in range(0, (token_count - size) + 1):
             span = doc.tokens[i:i + size].span
-            if span not in pos_entity_spans:
+            if span not in pos_entity_spans:#确保负样本实体不在正样本实体中
                 neg_entity_spans.append(span)
                 neg_entity_sizes.append(size)
 
     # sample negative entities
     neg_entity_samples = random.sample(list(zip(neg_entity_spans, neg_entity_sizes)),
-                                       min(len(neg_entity_spans), neg_entity_count))
+                                       min(len(neg_entity_spans), neg_entity_count))#选择指定数量的负样本实体，最小值为负样本实体总数和所需负样本数量
     neg_entity_spans, neg_entity_sizes = zip(*neg_entity_samples) if neg_entity_samples else ([], [])
 
     neg_entity_masks = [create_entity_mask(*span, context_size) for span in neg_entity_spans]
     neg_entity_types = [0] * len(neg_entity_spans)
 
-    # negative relations
+    # negative relations  关系负样本的构建
     # use only strong negative relations, i.e. pairs of actual (labeled) entities that are not related
     neg_rel_spans = []
 
@@ -69,15 +69,15 @@ def create_train_sample(doc, neg_entity_count: int, neg_rel_count: int, max_span
             # entity pairs that are related according to gt
             if s1 != s2 and (s1, s2) not in pos_rel_spans:
                 neg_rel_spans.append((s1, s2))
-
+            #当实体对不是同一个实体且不在正样本关系跨度中时，添加为负样本关系跨度，选择正样本实体对之间不存在关系的实体对作为负样本关系，也就是强负样本关系，不要那一个正确的样本的其余组合
     # sample negative relations
-    neg_rel_spans = random.sample(neg_rel_spans, min(len(neg_rel_spans), neg_rel_count))
+    neg_rel_spans = random.sample(neg_rel_spans, min(len(neg_rel_spans), neg_rel_count))#选择指定数量的负样本关系，最小值为负样本关系总数和所需负样本数量
 
     neg_rels = [(pos_entity_spans.index(s1), pos_entity_spans.index(s2)) for s1, s2 in neg_rel_spans]
     neg_rel_masks = [create_rel_mask(*spans, context_size) for spans in neg_rel_spans]
     neg_rel_types = [(0,) * (rel_type_count-1)] * len(neg_rel_spans)
 
-    # merge
+    # merge 正负样本的合并
     entity_types = pos_entity_types + neg_entity_types
     entity_masks = pos_entity_masks + neg_entity_masks
     entity_sizes = pos_entity_sizes + list(neg_entity_sizes)
