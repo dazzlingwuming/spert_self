2cdf784f94c075e316097419422a1c0f40100c3a
lihaodong <2409261729@qq.com>
2025-10-23 00:40:03 +0800

    模型结构

--- spert.py
+++ spert.py
@@ -3,6 +3,7 @@ import argparse
 from args import train_argparser, eval_argparser, predict_argparser
 from config_reader import process_configs
 from spert import input_reader
+from spert.spert_predictor import SpertPredictor
 from spert.spert_trainer import SpERTTrainer
 
 
@@ -38,6 +39,28 @@ def __predict(run_args):
     trainer.predict(dataset_path=run_args.dataset_path, types_path=run_args.types_path,
                     input_reader_cls=input_reader.JsonPredictionInputReader)
 
+def _local_predict():
+    arg_parser = predict_argparser()
+    process_configs(target=__local_predict, arg_parser=arg_parser,mutil_process=False)
+
+def __local_predict(run_args):
+    model_predict = SpertPredictor(types_path = run_args.types_path,
+                             model_type = run_args.model_type,
+                             model_path = run_args.model_path,
+                             cache_path = run_args.cache_path,
+                             tokenizer_path = run_args.tokenizer_path,
+                             max_span_size = run_args.max_span_size,
+                             max_pairs = run_args.max_pairs,
+                             prop_drop= run_args.prop_drop,
+                             size_embedding = run_args.size_embedding,
+                             rel_filter_threshold = run_args.rel_filter_threshold)
+    #进行预测
+    while True:
+        text = input("请输入文本，以空格隔开")
+        if text == '':
+            break
+        predictions = model_predict.predict(text)
+        print(predictions)
 
 if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser(add_help=False)
@@ -50,5 +73,7 @@ if __name__ == '__main__':
         _eval()
     elif args.mode == 'predict':
         _predict()
+    elif args.mode == 'local_predict':
+        _local_predict()
     else:
         raise Exception("Mode not in ['train', 'eval', 'predict'], e.g. 'python spert.py train ...'")
