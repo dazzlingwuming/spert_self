37481785d84afaf63839e71c571be099e0d2277e
lihaodong <2409261729@qq.com>
2025-10-24 23:42:16 +0800

    neo4j模块

--- py2neo_demo/neo4j_test.py
+++ py2neo_demo/neo4j_test.py
@@ -20,6 +20,18 @@ class HelloWorldExample:
                         "RETURN a.message + ', from node ' + id(a)", message=message)
         return result.single()[0]
 
+def t1():
+    with GraphDatabase.driver("bolt://localhost:7687") as driver:
+        driver.verify_connectivity()
+        '''
+        1.非事务性操作：适合执行简单的读写操作，不需要复杂的事务管理：
+            record, summary , key = driver.execute_query("MATCH (n:'类型‘ {类型名称 ：$name}) RETURN n LIMIT 1" , name="动作")
+            record, summary , key 分别是查询结果、查询摘要和返回字段名列表。
+        2.事务性操作：适合需要多步操作的复杂事务，可以确保:
+            with self.driver.session(database = "neo4j") as session:
+                greeting = session.execute_write(self._create_and_return_greeting, message)
+                print(greeting)
+        '''
 
 if __name__ == "__main__":
     greeter = HelloWorldExample("bolt://:7687", "neo4j", "password")
